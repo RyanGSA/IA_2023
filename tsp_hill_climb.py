e0a9aadbf94eaec368e8dfa65fb75b33e00a45a1
alehxalves <alehxalves@gmail.com>
2023-10-28 11:53:45 -0300

    comments

--- tsp_hill_climb.py
+++ tsp_hill_climb.py
@@ -3,11 +3,13 @@ import random
 
 from Cidades import getCidades
 
+# Calcular distancia entre duas cidades.
 def calcular_distancia(cidade1, cidade2):
     x1, y1 = (cidade1.xCoord, cidade1.yCoord)
     x2, y2 = (cidade2.xCoord, cidade2.yCoord)
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
+# Calcular o custo total do circuito
 def custo_total_circuito(circuito):
     total = 0
     for i in range(len(circuito)):
@@ -16,6 +18,7 @@ def custo_total_circuito(circuito):
         total += calcular_distancia(cidade_atual,cidade_seguinte)
     return total
 
+# Gerar vizinhos
 def getVizinhos(circuito):
   vizinhos = []
   for i in range(len(circuito)):
@@ -26,12 +29,14 @@ def getVizinhos(circuito):
   return vizinhos
 
 
+# Gerar um circuito inicial
 def circuito_inicial(cidades):
     circuito = list(range(len(cidades)))
     random.shuffle(circuito)
     print("cir inicial: ", circuito)
     return circuito
 
+# Algoritmo subida da encosta
 def subida_encosta(cidades):
   melhor_circuito = circuito_inicial(cidades)
   melhor_custo = custo_total_circuito(melhor_circuito)
@@ -48,6 +53,7 @@ def subida_encosta(cidades):
 
   return melhor_circuito, melhor_custo
 
+ # Main
 if __name__ == "__main__":
   cidades = getCidades()
 
